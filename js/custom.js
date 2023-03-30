86b1236912637b4af8998546515d893288fd0f57
Nínive Helen <ninivehelen@gmail.com>
2023-03-30 11:21:00 -0300

    Atualizaçao do potfolio online

--- js/custom.js
+++ js/custom.js
@@ -34,3 +34,13 @@
     $('.social-links a').tooltip();
 
 })(jQuery);
+
+var btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
+
+var btn = $("#back-to-top");
+btn.click(function() {
+  $('html, body').animate({scrollTop:0}, 'slow');
+});
